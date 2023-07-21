dea32b864ac0bc8cf7e8d8531f9747d5846a0b41
twystd <twystd@users.noreply.github.com>
2023-07-21 12:12:56 -0700

    erlang: reworking encoder for codegen

--- bindings/erlang/encoder.erl
+++ bindings/erlang/encoder.erl
@@ -3,7 +3,18 @@
 -export([ get_controller_request/1 ]).
 
 get_controller_request (Controller) ->
-    << 16#17, 16#94, 16#00, 16#00, 
-       Controller:4/little-unsigned-integer-unit:8,
-       0:56/little-unsigned-integer-unit:8
-    >>.
+    P1 = <<16#17, 16#94, 16#00, 16#00, 0:60/little-unsigned-integer-unit:8>>,
+    Packet = pack_uint32(Controller, P1, 4),
+
+    Packet.
+
+pack_uint32(V, Packet, Offset) ->
+    B = pad(binary:encode_unsigned(V,little), 4),
+    <<P:Offset/binary,_:32,R/binary>> = Packet,
+    <<P/binary,B/binary,R/binary>>.
+
+
+pad(B, N) ->
+    Padding = 8*(N - byte_size(B)),
+    << <<0:Padding>>/binary, B/binary>>.
+
